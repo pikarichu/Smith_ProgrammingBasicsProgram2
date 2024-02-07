51d34cd89af7811c1cce3c2c0bde9f3577db06d4
Sam Smith <pikarichu@gmail.com>
2024-02-07 13:24:02 -0500

    adding comments

--- ProgrammingBasicsProgram2.cpp
+++ ProgrammingBasicsProgram2.cpp
@@ -1,5 +1,6 @@
 // ProgrammingBasicsProgram2.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+// This program is designed to take salary data from one file labeled SalaryData.txt, apply the increase in pay
+// and output the new salary to a file labeled SalaryOut.txt
 
 #include <iostream>
 #include <fstream>
